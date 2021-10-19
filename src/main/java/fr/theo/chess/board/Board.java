78eed06ff943977b8a34b427eebd4dbeaa93445a
theoBattani <84023706+theoBattani@users.noreply.github.com>
2021-10-19 20:21:18 +0200

    ...

--- src/main/java/fr/theo/chess/board/Board.java
+++ src/main/java/fr/theo/chess/board/Board.java
@@ -1,3 +1,4 @@
+
 package fr.theo.chess.board;
 
 public class Board {
@@ -5,7 +6,18 @@ public class Board {
   private int[] array;
 
   public Board() {
-    this.array = new int[]
+    this.array = new int[64];
   }
 
+  public int[] getArray() {return this.array;}
+  public int get(int index) {return this.getArray()[index];}
 }
+
+
+
+
+
+
+
+
+
